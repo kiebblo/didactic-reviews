ca289834a8567b9c0569535c9831f7f73471ff6e
kiebblo <kibarteta@gmail.com>
2022-05-11 18:33:24 +0200

    middleware and controller
    User controller set up, to have authentication and login token

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
+require("./config/db");
 const express = require("express");
 const app =  express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-require("./config/db")
-
-
+const RouteUser = require("./route/UserRoute");
+app.use("/user", RouteUser);
 
 
 app.listen(process.env.PORT, () => console.log(`THe server is running on port ${process.env.PORT}`));
